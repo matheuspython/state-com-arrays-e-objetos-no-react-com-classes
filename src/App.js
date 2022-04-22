ff74fa0c9e3bf59fb417d76cf4d5bf71a37600b9
matheuspython <ruivorobado@gmail.com>
2022-04-22 14:26:17 -0300

    estrutura inicial do app

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
-import logo from './logo.svg';
-
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component{
-    state = {
-      name: 'matheus souza',
-      counter: 0
-    }
+    // state = {
+    //   name: 'matheus souza',
+    //   counter: 0
+    // }
 
-  handlePClick = ()=>  this.setState({ name: 'outro nome' })
-  
-  handleAClick = event =>{
-    event.preventDefault()
-    const { counter } = this.state
-    this.setState({ counter: counter + 1})
-  }
-  
   render(){
-    const { name, counter } = this.state
+    // const { name, counter } = this.state
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-          <p onClick={ this.handlePClick }>
-           { name } clicou { counter } vezes
-          </p>
-          <a
-           onClick={ this.handleAClick }
-           className="App-link"
-           href="https://reactjs.org"
-           target="_blank"
-           rel="noopener noreferrer"
-          > 
-           Este é o link
-          </a>
-        </header>
+        teste
      </div>
     )
   }
