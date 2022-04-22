21c4291c073c817e404e0d276bc20f356b4e451c
matheuspython <ruivorobado@gmail.com>
2022-04-22 14:42:45 -0300

    exemplo feito

--- src/App.js
+++ src/App.js
@@ -2,16 +2,36 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component{
-    // state = {
-    //   name: 'matheus souza',
-    //   counter: 0
-    // }
+    state = {
+      posts: [
+        {
+          id: 1,
+          title: 'O titulo 1',
+          body: 'O corpo 1',
+        },
+        {
+          id: 2,
+          title: 'O titulo 2',
+          body: 'O corpo 2',
+        },{
+          id: 3,
+          title: 'O titulo 3',
+          body: 'O corpo 3',
+        },
+      ],
+
+    }
 
   render(){
-    // const { name, counter } = this.state
+    const { posts } = this.state
     return (
       <div className="App">
-        teste
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1 key={post.id}>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
      </div>
     )
   }
